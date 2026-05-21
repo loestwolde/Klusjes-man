657b97453cbb733ad4e0ce829c0ea2fe60f24f84
Onno <onno.bruil@student.graafschapcollege.nl>
2026-05-21 15:04:12 +0200

    toevoegen aangepast

--- public/toevoegen.php
+++ public/toevoegen.php
@@ -17,6 +17,22 @@
     <input type="text" name="telefoon" />
     <br />
 
+    <label for="woonplaats">Woonplaats:</label>
+    <input type="text" name="woonplaats" />
+    <br />
+
+    <label for="straat">Straat:</label>
+    <input type="text" name="straat" />
+    <br />
+
+    <label for="huisnummer">Huisnummer:</label>
+    <input type="text" name="huisnummer" />
+    <br />
+
+    <label for="huisnummertoevoeging">Huisnummertoevoeging:</label>
+    <input type="text" name="huisnummertoevoeging" />
+    <br />
+
     <input type="submit" name="saveCustomer"/>
 </form>
 
@@ -29,15 +45,21 @@ if(isset($_POST['saveCustomer'])){
     $achternaam = $_POST['achternaam'];
     $aanhef = $_POST['aanhef'];
     $email = $_POST['email'];
-   
+    $woonplaats = $_POST['woonplaats'];
+    $straat = $_POST['straat'];
+    $huisnummer = $_POST['huisnummer'];
+    $huisnummer_toevoeging = $_POST['huisnummertoevoeging'];
     $phone = $_POST['telefoon'];
     
     // klant opslaan
     $newCustomer = new Customer();
-    if($newCustomer->saveCustomer($voorletters, $achternaam, $aanhef, $email, $phone))
+    if($newCustomer->saveCustomer($voorletters, $achternaam, $aanhef, $email, $phone, $woonplaats, $straat, $huisnummer, $huisnummer_toevoeging))
     {
+        $$adresId = $this->adresToevoegenAanKlant($lastId, $woonplaats, $straat, $huisnummer, $huisnummer_toevoeging);
+        $newCustomer->klantAanAdresKoppelen($newCustomer->getlastid(), $adresId);
         echo "Klant is opgeslagen";
         header("Location: index.php");
+        
     } else {
         echo "Klant is niet opgeslagen";
     }
--- src/customer.php
+++ src/customer.php
@@ -12,19 +12,34 @@ class Customer extends Database
         $query = "SELECT * FROM klanten WHERE klant_id = ?";
         return parent::voerQueryUit($query, [$klantId]);
     }
-    public function saveCustomer($voorletters, $achternaam, $aanhef, $email, $telefoon)
+    public function saveCustomer($voorletters, $achternaam, $aanhef, $email, $telefoon, $woonplaats, $straat, $huisnummer, $huisnummer_toevoeging)
     {
         $lastId = $this->getlastid();
         // controleren of alle velden gevuld zijn
-        if($voorletters == "" || $achternaam == "" || $aanhef == "" || $email == "" || $telefoon == "")
+        if($voorletters == "" || $achternaam == "" || $aanhef == "" || $email == "" || $telefoon == "" || $woonplaats == "" || $straat == "" || $huisnummer == "" || $huisnummer_toevoeging == "")
         {
             return false;
         }
 
-        // HIER AANVULLEN
+        // Adres toevoegen
+        $adresId = $this->adresToevoegenAanKlant($woonplaats, $straat, $huisnummer, $huisnummer_toevoeging);
 
-        
+        // Klant aan adres koppelen
+        $this->klantAanAdresKoppelen($lastId, $adresId);
+
         $query = "INSERT INTO klanten (voorletters, achternaam, aanhef, email, telefoon) VALUES (?, ?, ?, ?, ?)";
-        return parent::voerQueryUit($query, [$voorletters, $achternaam, $aanhef, $email,  $telefoon]);
+        return parent::voerQueryUit($query, [$voorletters, $achternaam, $aanhef, $email, $telefoon]);
     }
+
+     public function adresToevoegenAanKlant($woonplaats, $straat, $huisnummer, $huisnummer_toevoeging)
+     {
+        $query = "INSERT INTO adressen (woonplaats, straat, huisnummer, huisnummer_toevoeging) VALUES (?, ?, ?, ?)";
+        return parent::voerQueryUit($query, [$woonplaats, $straat, $huisnummer, $huisnummer_toevoeging]);
+     }
+
+     public function klantAanAdresKoppelen($klantId, $adresId)
+     {
+        $query = "INSERT INTO klant_adres (klant_id, adres_id, startdatum) VALUES (?, ?, ?)";
+        return parent::voerQueryUit($query, [$klantId, $adresId, date('Y-m-d')]);
+     }
 }
